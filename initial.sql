c1c36b3de21d5169b589b27b736cd001d2c62199
jakethesnake0619 <59973256+jakethesnake0619@users.noreply.github.com>
2021-10-06 22:39:54 -0400

    Create initial.sql
    First draft of base tables

---
+++ initial.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS Items;
+
+CREATE TABLE items(
+  PRIMARY KEY itemID CHAR(10),
+  name CHAR(20),
+  price DECIMAL(7,2),
+  image CHAR(255),
+  timesBought INT(5)
+);
+
+DROP TABLE IF EXISTS Users;
+
+CREATE TABLE users(
+  PRIMARY KEY userID CHAR(10),
+  name CHAR(30),
+  mostBought CHAR(10),
+  amountBought INT(5),
+  moneySpent DECIMAL(7,2)
+);
